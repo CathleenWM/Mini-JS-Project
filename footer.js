1b550d0216216595c7b413ec414b3423efbb5695
Cassovich <cassws00@gmail.com>
2022-09-02 10:54:58 +0200

    js file and footer html

---
+++ footer.js
@@ -0,0 +1,20 @@
+
+var changeLanguageBox = document.getElementById("language_select");
+var languageList = document.getElementById("language_list");
+var windowPane = document.getElementById("window_pane");
+console.log(languageList);
+
+function openLanguageSelection(){
+    console.log("open");
+    languageList.style.display = "flex";
+    windowPane.style.display = "block";
+}
+
+function closeLanguageSelection(){
+    console.log("closed");
+    languageList.style.display = "none";
+    windowPane.style.display = "none";
+}
+
+
+windowPane.addEventListener("click", closeLanguageSelection);
