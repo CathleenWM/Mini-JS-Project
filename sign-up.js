97ccc87141e8a0c3fa8e42d9b0755b8b03aa5079
CathleenWM <cathleen5068@gmail.com>
2022-09-04 21:59:42 +0200

    validation of age started

--- sign-up.js
+++ sign-up.js
@@ -1,6 +1,7 @@
 var name1Error = document.getElementById('name1-error');
 var name2Error = document.getElementById('name2-error');
 var ageError = document.getElementById('age-error');
+var olderError = document.getElementById('older-error');
 var emailError = document.getElementById('email-error');
 var passwordError = document.getElementById('password-error');
 
@@ -32,5 +33,25 @@ function validateName2(){
     }else{
         name2Error.innerHTML = '';
         return true;
+    }
+}
+
+// Validation for Age
+function validateAge(){
+    var yourAge = document.getElementById('age').value;
+
+    // User has to enter an age
+    if(yourAge.length == 0){
+        ageError.innerHTML = 'Required';
+        return false;
+    }
+
+    // The age must be over 13
+    if(isNaN(yourAge) || yourAge < 13){
+        olderError.innerHTML = 'In order to use Flickr, you must be 13 or older';
+        return false;
+    }else{
+        olderError.innerHTML = '';
+        return true; // NOTE error msg wont dissapir and nether will the ageError msg
     }
 }
